e4830d90fcbcb472136fc0cae835f7026f41c5ef
Viet Tien <vdang074@uottawa.ca>
2022-10-20 23:57:51 -0400

    finished logout

--- app/src/main/java/com/app/mealer/WelcomeActivity.java
+++ app/src/main/java/com/app/mealer/WelcomeActivity.java
@@ -2,13 +2,33 @@ package com.app.mealer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class WelcomeActivity extends AppCompatActivity {
-
+    private FirebaseAuth mauth;
+    private Button logout_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        logout_btn=findViewById(R.id.logInButton);
+        logout_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+    }
+
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(WelcomeActivity.this,LoginForm.class));
     }
 }
