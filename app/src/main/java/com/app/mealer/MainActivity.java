4f229b1e4eb2810c5697ac5fcfb7b87dd6b58370
Viet Tien <vdang074@uottawa.ca>
2022-10-20 23:46:53 -0400

    atachlogin and signup

--- app/src/main/java/com/app/mealer/MainActivity.java
+++ app/src/main/java/com/app/mealer/MainActivity.java
@@ -1,19 +1,38 @@
 package com.app.mealer;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button signin_btn,signup_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signin_btn=findViewById(R.id.LogInButton);
+        signup_btn=findViewById(R.id.SignUpButton);
+
+
+        signin_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,LoginForm.class));
+            }
+        });
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,SignupForm.class));
+            }
+        });
     }
 
-    public void OnOpenLogin(View view){}
 
-    public void OnOpenSignup(View view){}
 
 }
