72e9b0b8a655f6c47c8064693106627a10c299fc
Zaheer MacDonald <gmacd083@uottawa.ca>
2022-12-07 14:02:36 -0500

    merging

---
+++ app/src/main/java/com/app/mealer/SearchListActivity.java
@@ -0,0 +1,14 @@
+package com.app.mealer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_list);
+    }
+}
